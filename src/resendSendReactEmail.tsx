b14361feeaa7594ffca2991267972713410da1c4
Dan <dkp.github@pm.me>
2023-08-31 14:08:58 +0100

    Resend React example (#6)
    * resend-react
    
    * React emails are working without javascript file
    
    * Removed tsc:email line
    
    ---------
    
    Co-authored-by: Rishi Raj Jain <rishi18304@iiitd.ac.in>

---
+++ src/resendSendReactEmail.tsx
@@ -0,0 +1,105 @@
+// src/resendSendReactEmail.tsx
+
+import { TriggerClient, eventTrigger } from "@trigger.dev/sdk";
+import { Resend } from "@trigger.dev/resend";
+import { z } from "zod";
+
+// react-email imports
+import { Html } from "@react-email/html";
+import { Head } from "@react-email/head";
+import { Text } from "@react-email/text";
+import { Button } from "@react-email/button";
+import { Section } from "@react-email/section";
+import { Preview } from "@react-email/preview";
+import { Container } from "@react-email/container";
+
+const client = new TriggerClient({ id: "jobs-showcase" });
+
+const resend = new Resend({
+  id: "resend",
+  apiKey: process.env.RESEND_API_KEY!,
+});
+
+// Email styling
+const container = {
+  width: "480px",
+  margin: "0 auto",
+  padding: "20px 0 48px",
+};
+
+const title = {
+  fontSize: "24px",
+  lineHeight: 1.25,
+};
+
+const section = {
+  padding: "24px",
+  border: "solid 1px #dedede",
+  borderRadius: "5px",
+  textAlign: "center" as const,
+};
+
+const text = {
+  margin: "0 0 10px 0",
+  textAlign: "left" as const,
+};
+
+const button = {
+  fontSize: "14px",
+  backgroundColor: "#28a745",
+  color: "#fff",
+  lineHeight: 1.5,
+  borderRadius: "0.5em",
+  padding: "0.75em 1.5em",
+};
+
+function BasicEmail({ name, text }: { name: string; text: string }) {
+  return (
+    <Html>
+      <Head />
+      <Preview>Welcome to Acme Inc!</Preview>
+      <Container style={container}>
+        <Section style={section}>
+          <Text>Hey {name}!</Text>
+          <Text>{text}</Text>
+          <Button style={button}>Get started</Button>
+        </Section>
+      </Container>
+    </Html>
+  );
+}
+
+// This job sends a basic email to a 'to' email address, a 'subject', a 'text' field and a 'from' email address.
+client.defineJob({
+  id: "resend-send-react-email",
+  name: "Resend: send react email",
+  version: "1.0.0",
+  trigger: eventTrigger({
+    name: "send.email",
+    schema: z.object({
+      to: z.string(),
+      subject: z.string(),
+      text: z.string(),
+      name: z.string(),
+      // The 'from' email address must be a verified domain in your Resend account.
+      from: z.string(),
+    }),
+  }),
+  integrations: {
+    resend,
+  },
+  run: async (payload, io, ctx) => {
+    await io.resend.sendEmail("send-email", {
+      to: payload.to,
+      subject: payload.subject,
+      text: payload.text,
+      from: payload.from,
+      // BasicEmail is the custom React component that will be used to style the email
+      react: <BasicEmail name={payload.name} text={payload.text} />,
+    });
+  },
+});
+
+// These lines can be removed if you don't want to use express
+import { createExpressServer } from "@trigger.dev/express";
+createExpressServer(client);
